809413eb09cbe6d9bd13470f0931af7edbfe163d
TruongQuangMin <minhvvn01@gmail.com>
2024-10-28 17:14:47 +0700

    Update code after merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
 import { WishlistModule } from './wishlist/wishlist.module';
 import { CartModule } from './cart/cart.module';
+import { PrismaService } from './prisma.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [ProductModule, WishlistModule, CartModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,PrismaService],
 })
 export class AppModule {}
