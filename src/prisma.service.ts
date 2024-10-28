746501647042536a078dab72035621944f2a69f7
thai <155620390+thaihuynh123@users.noreply.github.com>
2024-10-28 15:24:11 +0700

    Revert "update prisma"
    This reverts commit f9f1d8db82277c4618fc21d6f78e76fc6e23ac4b.

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,13 +1,17 @@
-import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleDestroy {
-  constructor() {
-    super();
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
   }
 
-  async onModuleDestroy() {
-    await this.$disconnect();
+
+  async enableShutdownHooks(app: INestApplication) {
+    process.on('beforeExit', async () => {
+      console.log('App is about to exit. Closing NestJS app...');
+      await app.close(); // Đóng ứng dụng NestJS
+    });
   }
 }
