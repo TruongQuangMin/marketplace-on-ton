df3c1ac9ec558e69a4fe5d07c11a46c9b27b33ec
thai <hpthkl@gmail.com>
2024-10-28 16:38:38 +0700

    Reapply "update logic"
    This reverts commit 1110bb23cb3cb0643d807a44a3e4c531c7053b26.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 // import { UserController } from './user/user.controller';
 // import { ProductController } from './product/product.controller';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule,UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,PrismaService],
 })
 export class AppModule {}
---
+++ util/supabaseUtil.ts
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { v4 as uuidv4 } from 'uuid';
+const SUPABASE_STORAGE_URL = process.env.SUPABASE_STORAGE_URL!;
+const SUPABASE_BASE_URL = process.env.SUPABASE_BASE_URL!;
+const SUPABASE_NON_PUBLIC_KEY = process.env.SUPABASE_NON_PUBLIC_KEY!;
+
+const supabase: SupabaseClient = createClient(
+  SUPABASE_BASE_URL,
+  SUPABASE_NON_PUBLIC_KEY,
+);
+
+const SupabaseUtil = {
+  async Upload(file: Express.Multer.File): Promise<string> {
+    const fileName = `${file.originalname}`;
+    const { data, error } = await supabase.storage
+      .from('marketplace-on-ton')  
+      .upload(fileName, file.buffer, {  
+        cacheControl: '3600',
+        contentType: file.mimetype,
+        upsert: false,
+      });
+
+    if (error) throw new Error(`Upload failed: ${error.message}`);
+    return `${SUPABASE_STORAGE_URL}/marketplace-on-ton/${fileName}`;
+  },
+
+
+  async Delete(storage_path_arr: string[]): Promise<void> {
+    const { data, error } = await supabase.storage
+      .from('marketplace-on-ton')
+      .remove(storage_path_arr);
+
+    if (error) {
+      console.error('Delete Error:', error);
+    } else {
+      console.log('Delete Success:', data);
+    }
+  },
+};
+
+export default SupabaseUtil;
