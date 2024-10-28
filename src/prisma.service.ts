f9f1d8db82277c4618fc21d6f78e76fc6e23ac4b
thai <106147688+thaihpgcc200197@users.noreply.github.com>
2024-10-28 15:18:03 +0700

    update prisma

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,17 +1,13 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
+export class PrismaService extends PrismaClient implements OnModuleDestroy {
+  constructor() {
+    super();
   }
 
-
-  async enableShutdownHooks(app: INestApplication) {
-    process.on('beforeExit', async () => {
-      console.log('App is about to exit. Closing NestJS app...');
-      await app.close(); // Đóng ứng dụng NestJS
-    });
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
