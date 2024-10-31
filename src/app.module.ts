ccab942b40f24e793dac42af1b9ca2b337d1576c
TruongQuangMin <minhvvn01@gmail.com>
2024-10-31 14:43:35 +0700

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WishlistModule } from './wishlist/wishlist.module';
-import { CartModule } from './cart/cart.module';
 import { PrismaService } from './prisma.service';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ProductModule, WishlistModule, CartModule],
+  imports: [ProductModule, WishlistModule, UserModule],
   controllers: [AppController],
   providers: [AppService,PrismaService],
 })
