3d211c82e9656ebcd4db3af8a86e3fcf16cfd2ed
duy-bean <167979475+MR-bear123@users.noreply.github.com>
2024-11-12 15:11:45 +0700

    Update cart

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,17 +4,16 @@ import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { PrismaService } from 'src/prisma.service';
 import 'dotenv/config';
+import { CartsModule } from 'src/carts/carts.module';
 import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD, Reflector } from '@nestjs/core';
 import { AuthGuard } from './auth.guard';
 const SECRET = process.env.SECRET!;
-import { CacheModule } from '@nestjs/cache-manager';
-
 
 @Module({
   imports: [
     UserModule,
-    CacheModule.register(),
+    CartsModule,
     JwtModule.register({ secret: SECRET }),
   ],
   controllers: [AuthController],
