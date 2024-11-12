1d8be0d6fa5cdc2236de67d4545375a293258a58
thai <155620390+thaihuynh123@users.noreply.github.com>
2024-11-12 13:27:18 +0700

    upadate

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,6 @@ import {
   // InternalServerErrorException,
   UnauthorizedException,
 } from '@nestjs/common';
-import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import { directus_users } from '@prisma/client';
 import { UserService } from 'src/user/user.service';
