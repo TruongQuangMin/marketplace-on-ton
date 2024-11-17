33c5704035978ea0c1f335941fef2a911c2ed0c9
duy-bean <167979475+MR-bear123@users.noreply.github.com>
2024-11-17 22:40:44 +0700

    Duy Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,8 +8,9 @@ import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { HttpModule } from '@nestjs/axios';
+import { OrdersModule } from './orders/orders.module';
 @Module({
-  imports: [ProductModule, WishlistModule, UserModule, CartsModule,AuthModule, HttpModule],
+  imports: [ProductModule, WishlistModule, UserModule, CartsModule,AuthModule, HttpModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService,PrismaService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import session from 'express-session';
+import { ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
 
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
-  app.use(
-    session({
-      secret: 'TruongQuangMinh12102002', // Chuỗi bí mật cho session
-      resave: false,
-      
-      saveUninitialized: false,
-      cookie: {
-        maxAge: 24 * 60 * 60 * 1000, // Thời gian tồn tại của session (1 ngày)
-        secure: false,
-      },
-    }),
-  );
-
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
